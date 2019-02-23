369e543f37ac5552623dae836bed382c9f0f6b80
Tatskov99 <yaroslav.tatskov@gmail.com>
2019-02-23 16:24:35 +0200

    removed whitespaces

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-item">
+      <router-link to="/">
+        <font-awesome-icon icon="home" />
+      </router-link>
+    </div>
+    <div class="sidebar-item">
+      <router-link to="/page1">
+        <font-awesome-icon icon="user" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Sidebar"
+  }
+</script>
+
+<style scoped>
+  .sidebar {
+    width: 110px;
+    height: 100%;
+    position: fixed;
+    background: white;
+  }
+</style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,97 +2,95 @@
 
 <form action="" @submit.prevent="registerUser">
     <div class="select-photo">
-       <button type="submit" class="btn btn-primary">Select photo</button>  
+       <button type="submit" class="btn btn-primary">Select photo</button>
     </div>
-        <div class="profile"> 
+        <div class="profile">
         <div class=" ">
-            
-                
-            <div class="row align-items-start center "> 
+            <div class="row align-items-start center ">
                 <div class="formg-group  mt ml">
                         <label  for="first-name"></label>
-                        <input  type="first-name" 
-                                class="form-control" 
-                                id="first-name"  
-                                placeholder="First name" 
-                                required  
+                        <input  type="first-name"
+                                class="form-control"
+                                id="first-name"
+                                placeholder="First name"
+                                required
                                 v-model="user.firstName">
                 </div>
                 <div class="formg-group  mt">
                         <label  for="country"></label>
-                        <input  type="country" 
-                                class="form-control" 
-                                id="country" 
+                        <input  type="country"
+                                class="form-control"
+                                id="country"
                                 placeholder="country "
-                                required 
+                                required
                                 v-model="user.country">
                 </div>
                 <div class="formg-group mt">
                         <label  for="poste-code"></label>
-                        <input  type="poste-code"   
-                                class="form-control" 
-                                id="poste-code"  
-                                placeholder="Poste code" 
-                                required 
+                        <input  type="poste-code"
+                                class="form-control"
+                                id="poste-code"
+                                placeholder="Poste code"
+                                required
                                 v-model="user.posteCode">
                 </div>
-            </div>   
+            </div>
         </div>
         <div class=" ">
-            <div class="row align-items-start"> 
+            <div class="row align-items-start">
                 <div class="formg-group ml">
                         <label  for="last-name"></label>
-                        <input  type="last-name" 
-                                class="form-control" 
-                                id="last-name" 
+                        <input  type="last-name"
+                                class="form-control"
+                                id="last-name"
                                 placeholder="Last name "
-                                required 
+                                required
                                 v-model="user.lastName">
                 </div>
                 <div class="formg-group ">
                         <label  for="city"></label>
-                        <input  type="city" 
+                        <input  type="city"
                                 class="form-control"
-                                id="city" 
-                                placeholder="City " 
-                                required 
+                                id="city"
+                                placeholder="City "
+                                required
                                 v-model="user.city">
-                                 
-   
+
+
                 </div>
-            </div>   
+            </div>
         </div>
          <div class="">
-            <div class="row align-items-start"> 
+            <div class="row align-items-start">
                 <div class="formg-group ml">
                         <label  for="phone-number"></label>
-                        <input  type="tel" 
-                                class="form-control" 
-                                id="phone-number"  
-                                placeholder="Phone number" 
-                                required 
+                        <input  type="tel"
+                                class="form-control"
+                                id="phone-number"
+                                placeholder="Phone number"
+                                required
                                 v-model="user.phoneNumber">
                 </div>
                 <div class="formg-group ">
                         <label  for="address"></label>
-                        <input  type="address" 
-                                class="form-control" 
-                                id="address" 
-                                placeholder="Address " 
-                                required 
+                        <input  type="address"
+                                class="form-control"
+                                id="address"
+                                placeholder="Address "
+                                required
                                 v-model="user.address">
                 </div>
-                
+
                     <div class="formg-group ">
                         <label  for="address"></label>
                         <button type="submit" class="btn btn-primary "> Save</button>
-                
+
                 </div>
-                
-            </div>   
+
+            </div>
         </div>
-      </div>   
-           
+      </div>
+
 </form>
 </template>
 
@@ -117,7 +115,7 @@ export default {
         registerUser() {
             if(this.user.password !== this.user.confirmPassword ||  this.user.password.length < 6 ) {
                 this.error = true;
-            } 
+            }
             else{
                 firebase.auth().createUserWithEmailAndPassword(this.user.email, this.user.password)
                     .then( () => {
@@ -137,14 +135,14 @@ export default {
     .profile{
         height: 414px;
         width: 1550px;
-        margin-left: -129px;
+        /*margin-left: -129px;*/
         background: white;
         margin-top: 156px;
     }
     button{
         float: right;
         width: 95px;
-        
+
     }
 
     .formg-group{
@@ -155,7 +153,7 @@ export default {
         margin-top: 20px;
     }
     .ml{
-        
+
     }
  .form-control{
      width: 320px;
